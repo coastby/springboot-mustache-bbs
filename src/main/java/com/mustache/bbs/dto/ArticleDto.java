d4abed1e6da88f09bcc171ea86509876066edc2d
JO YEJI <whdpwl2@gmail.com>
2022-11-08 11:56:08 +0900

    css 이쁘게

--- src/main/java/com/mustache/bbs/dto/ArticleDto.java
+++ src/main/java/com/mustache/bbs/dto/ArticleDto.java
@@ -5,13 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
-@AllArgsConstructor
 @Getter
 @ToString
 public class ArticleDto {
     private Long id;
     private String title;
     private String content;
+
+    public ArticleDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 
     public Article toEntity(){
         return new Article(title, content);
