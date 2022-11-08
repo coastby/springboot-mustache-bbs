12e253bdc4b7accb059127233a6e4f54c29e39c8
JO YEJI <whdpwl2@gmail.com>
2022-11-08 15:55:42 +0900

    부트스트랩 적용

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -4,6 +4,7 @@ import com.mustache.bbs.dto.ArticleDto;
 import com.mustache.bbs.entity.Article;
 import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -50,10 +52,12 @@ public class ArticleController {
     }
     @GetMapping("")
     public String index(){
-        return "redirect:/article/list";
+        return "redirect:/articles/list";
     }
     @GetMapping("/list")
-    public String list(){
-        return "list";
+    public String list(Model model){
+        List<Article> articleList = articleRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+        model.addAttribute("articleList", articleList);
+        return "articles/list";
     }
 }
